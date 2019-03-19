5b68a5d1ecc1e116aa17880e06c927c6635816b9
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2019-03-19 10:38:48 +0800

    update

--- install.sh
+++ install.sh
@@ -2,20 +2,19 @@
 HOME=~;
 echo $(pwd)
 
-if [! -d "${HOME}/.ssh"]; then
+if [ ! -d "${HOME}/.ssh" ]; then
     mkdir ${HOME}/.ssh
     chmod 700 ${HOME}/.ssh
 fi
 scp pkusz_domain.pub ${HOME}/.ssh/authorized_keys
 chmod 600 ${HOME}/.ssh/authorized_keys
 
-sudo su;
-HOME=~;
+HOME=/root;
 
 echo $(pwd)
-if [! -d "${HOME}/.ssh"]; then
-    mkdir ${HOME}/.ssh
-    chmod 700 ${HOME}/.ssh
+if [ ! -d "${HOME}/.ssh" ]; then
+    sudo mkdir ${HOME}/.ssh
+    sudo chmod 700 ${HOME}/.ssh
 fi
-scp pkusz_domain.pub ${HOME}/.ssh/authorized_keys
-chmod 600 ${HOME}/.ssh/authorized_keys
+sudo scp pkusz_domain.pub ${HOME}/.ssh/authorized_keys
+sudo chmod 600 ${HOME}/.ssh/authorized_keys
